302c1bddb7bd3781601eab3f77895b89b642de54
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-15 22:31:47 +0900

    Export function waitMutex with metadata and logs at 20250515_223144

---
+++ ida_exports/waitMutex_20250515_223144.c
@@ -0,0 +1,28 @@
+// --- Metadata ---
+// Function Name: waitMutex
+// Address: 0x401EFF
+// Exported At: 20250515_223144
+// Signature: unknown_signature
+// ---------------
+int __cdecl waitMutex(int maxtry)
+{
+  int v1; // esi
+  HANDLE v2; // eax
+  char Buffer[100]; // [esp+4h] [ebp-64h] BYREF
+
+  sprintf(Buffer, "%s%d", "Global\\MsWinZonesCacheCounterMutexA", 0);// 뮤텍스 이름
+  v1 = 0;
+  if ( maxtry <= 0 )
+    return 0;
+  while ( 1 )
+  {
+    v2 = OpenMutexA(1048576u, 1, Buffer);       // 중복 실행 방지 혹은 선행 작업 대기 
+    if ( v2 )
+      break;
+    Sleep(1000u);
+    if ( ++v1 >= maxtry )                       // 타임 아웃 = 조건 불충분 
+      return 0;
+  }
+  CloseHandle(v2);
+  return 1;                                     // 뮤텍스 존재 -> 실행 조건 만족 
+}
