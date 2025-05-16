b77369d939041a8fc745a29e845e9738728e9359
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-16 09:11:18 +0900

    Export function AESKey_struct_4012FD with metadata and logs at 20250516_091115

---
+++ ida_exports/AESKey_struct_4012FD_20250516_091115.c
@@ -0,0 +1,18 @@
+// --- Metadata ---
+// Function Name: AESKey_struct_4012FD
+// Address: 0x4012FD
+// Exported At: 20250516_091115
+// Signature: unknown_signature
+// ---------------
+_DWORD *__thiscall AESKey_struct_4012FD(_DWORD *this)
+{
+  AESKey_structMember1_4017DD(this + 4);
+  AESKey_structMember1_4017DD(this + 44);
+  AESKey_structMember2_402A46(this + 84);
+  this[306] = 0;
+  this[307] = 0;
+  this[308] = 0;
+  this[309] = 0;
+  *this = &off_4081D8;
+  return this;
+}
