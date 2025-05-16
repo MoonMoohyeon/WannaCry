aac2737ed4578b53213cd40b4b8db2375d5cd6ce
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-16 09:11:37 +0900

    Export function AESKey_structMember1_4017DD with metadata and logs at 20250516_091135

---
+++ ida_exports/AESKey_structMember1_4017DD_20250516_091135.c
@@ -0,0 +1,15 @@
+// --- Metadata ---
+// Function Name: AESKey_structMember1_4017DD
+// Address: 0x4017DD
+// Exported At: 20250516_091135
+// Signature: unknown_signature
+// ---------------
+char *__thiscall AESKey_structMember1_4017DD(char *this)
+{
+  *(this + 1) = 0;
+  *(this + 2) = 0;
+  *(this + 3) = 0;
+  *this = &off_4081EC;
+  InitializeCriticalSection((this + 16));
+  return this;
+}
