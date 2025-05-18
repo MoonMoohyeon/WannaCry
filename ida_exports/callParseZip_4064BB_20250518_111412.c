3d7bb7e079fd4404c118b8a182846e47cf836473
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-18 11:14:15 +0900

    Export function callParseZip_4064BB with metadata and logs at 20250518_111412

---
+++ ida_exports/callParseZip_4064BB_20250518_111412.c
@@ -0,0 +1,10 @@
+// --- Metadata ---
+// Function Name: callParseZip_4064BB
+// Address: 0x4064BB
+// Exported At: 20250518_111412
+// Signature: unknown_signature
+// ---------------
+int __cdecl callParseZip_4064BB(int a1, int a2, LPVOID lpBuffer, int a4, LPVOID a5, int a6, LPVOID a7, int a8)
+{
+  return parseCentralZipEntry_4061E0(a1, a2, 0, lpBuffer, a4, a5, a6, a7, a8);
+}
