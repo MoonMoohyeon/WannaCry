a6d15440afa19446962f544b2bd3195a882d1d0d
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-19 15:39:53 +0900

    Export function LoadLibraryA_402198 with metadata and logs at 20250519_153950

---
+++ ida_exports/LoadLibraryA_402198_20250519_153950.c
@@ -0,0 +1,10 @@
+// --- Metadata ---
+// Function Name: LoadLibraryA_402198
+// Address: 0x402198
+// Exported At: 20250519_153950
+// Signature: unknown_signature
+// ---------------
+HMODULE __stdcall LoadLibraryA_402198(LPCSTR lpLibFileName)
+{
+  return LoadLibraryA(lpLibFileName);
+}
