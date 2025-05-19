172c2109bfdbccaf64ede5d00bf74bc79d0e96d2
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-19 15:40:17 +0900

    Export function FreeLibrary_4021B2 with metadata and logs at 20250519_154015

---
+++ ida_exports/FreeLibrary_4021B2_20250519_154015.c
@@ -0,0 +1,10 @@
+// --- Metadata ---
+// Function Name: FreeLibrary_4021B2
+// Address: 0x4021B2
+// Exported At: 20250519_154015
+// Signature: unknown_signature
+// ---------------
+BOOL __cdecl FreeLibrary_4021B2(HMODULE hLibModule)
+{
+  return FreeLibrary(hLibModule);
+}
