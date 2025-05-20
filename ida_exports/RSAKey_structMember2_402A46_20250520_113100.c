a5b75796f1b1ade70a52a41051d15408aa183c00
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-20 11:31:03 +0900

    Export function RSAKey_structMember2_402A46 with metadata and logs at 20250520_113100

---
+++ ida_exports/RSAKey_structMember2_402A46_20250520_113100.c
@@ -0,0 +1,15 @@
+// --- Metadata ---
+// Function Name: RSAKey_structMember2_402A46
+// Address: 0x402A46
+// Exported At: 20250520_113100
+// Signature: unknown_signature
+// ---------------
+_BYTE *__thiscall RSAKey_structMember2_402A46(_BYTE *this)
+{
+  _BYTE *result; // eax
+
+  result = this;
+  this[4] = 0;
+  *this = &off_40BC7C;
+  return result;
+}
