bda5ba4738b80484a6f84d5d897a1d4d04f86072
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-20 11:31:24 +0900

    Export function RSAKey_struct_4012FD with metadata and logs at 20250520_113121

---
+++ ida_exports/RSAKey_struct_4012FD_20250520_113121.c
@@ -0,0 +1,18 @@
+// --- Metadata ---
+// Function Name: RSAKey_struct_4012FD
+// Address: 0x4012FD
+// Exported At: 20250520_113121
+// Signature: unknown_signature
+// ---------------
+_DWORD *__thiscall RSAKey_struct_4012FD(_DWORD *this)
+{
+  RSAKey_structMember1_4017DD(this + 4);
+  RSAKey_structMember1_4017DD(this + 44);
+  RSAKey_structMember2_402A46(this + 84);
+  this[306] = 0;
+  this[307] = 0;
+  this[308] = 0;
+  this[309] = 0;
+  *this = &off_4081D8;
+  return this;
+}
