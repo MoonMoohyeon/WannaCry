cefe70c8f058de7a73564811dd4fff3c92502622
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-22 12:29:12 +0900

    Export function StartAddress with metadata and logs at 20250522_122908

---
+++ ida_exports_t/StartAddress_20250522_122908.c
@@ -0,0 +1,10 @@
+// --- Metadata ---
+// Function Name: StartAddress
+// Address: 0x100029E0
+// Exported At: 20250522_122908
+// Signature: unknown_signature
+// ---------------
+void __stdcall __noreturn StartAddress(LPVOID lpThreadParameter)
+{
+  WannaCryFilePurger_100029F0((int)lpThreadParameter);
+}
