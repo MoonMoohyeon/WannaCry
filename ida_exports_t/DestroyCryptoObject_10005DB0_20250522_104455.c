7709f153b25e28cf0d7a2688f86d059d5fb396e6
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-22 10:44:57 +0900

    Export function DestroyCryptoObject_10005DB0 with metadata and logs at 20250522_104455

---
+++ ida_exports_t/DestroyCryptoObject_10005DB0_20250522_104455.c
@@ -0,0 +1,10 @@
+// --- Metadata ---
+// Function Name: DestroyCryptoObject_10005DB0
+// Address: 0x10005DB0
+// Exported At: 20250522_104455
+// Signature: unknown_signature
+// ---------------
+void __thiscall DestroyCryptoObject_10005DB0(_DWORD *this)
+{
+  *this = &off_1000ACBC;                        // 가상 테이블 초기화 
+}
