aa6c6e10cbb3a193e6329c47b634bf26539c56b1
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-22 12:38:45 +0900

    Export function ReleaseCryptoResources_10003BB0 with metadata and logs at 20250522_123842

---
+++ ida_exports_t/ReleaseCryptoResources_10003BB0_20250522_123842.c
@@ -0,0 +1,28 @@
+// --- Metadata ---
+// Function Name: ReleaseCryptoResources_10003BB0
+// Address: 0x10003BB0
+// Exported At: 20250522_123842
+// Signature: unknown_signature
+// ---------------
+int __thiscall ReleaseCryptoResources_10003BB0(_DWORD *this)
+{
+  HCRYPTPROV v2; // eax
+
+  if ( this[2] )
+  {
+    CryptDestroyKey(this[2]);
+    this[2] = 0;
+  }
+  if ( this[3] )
+  {
+    CryptDestroyKey(this[3]);
+    this[3] = 0;
+  }
+  v2 = this[1];
+  if ( v2 )
+  {
+    CryptReleaseContext(v2, 0);
+    this[1] = 0;
+  }
+  return 1;
+}
