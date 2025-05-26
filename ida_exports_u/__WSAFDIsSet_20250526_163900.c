0d5f403932db514a724c2350f7baaf5111c4ac09
quriquri <uuepr21@naver.com>
2025-05-26 16:39:05 +0900

    Export function __WSAFDIsSet with metadata and logs at 20250526_163900

---
+++ ida_exports_u/__WSAFDIsSet_20250526_163900.c
@@ -0,0 +1,11 @@
+// --- Metadata ---
+// Function Name: __WSAFDIsSet
+// Address: 0x4133E0
+// Exported At: 20250526_163900
+// Signature: unknown_signature
+// ---------------
+// attributes: thunk
+int __stdcall _WSAFDIsSet(SOCKET fd, fd_set *a2)
+{
+  return __WSAFDIsSet(fd, a2);
+}
