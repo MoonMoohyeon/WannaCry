a965d486735d051456850e3484505b704743811b
MoonMoohyeon <fancy@konkuk.ac.kr>
2025-05-27 18:27:09 +0900

    Export function isSecondParamZero_406EB0 with metadata and logs at 20250527_182706

---
+++ ida_exports_m/isSecondParamZero_406EB0_20250527_182706.c
@@ -0,0 +1,10 @@
+// --- Metadata ---
+// Function Name: isSecondParamZero_406EB0
+// Address: 0x406EB0
+// Exported At: 20250527_182706
+// Signature: unknown_signature
+// ---------------
+BOOL __cdecl isSecondParamZero_406EB0(int a1, int a2)
+{
+  return a2 == 0;
+}
